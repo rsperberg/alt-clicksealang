89722a2e01ceb05a46fc750e0f37ba8bf7dd96f0
rsperberg <rsperberg@gmail.com>
2015-03-13 16:14:31 -0400

    initial commit of code from ver 0.68a xpi

---
+++ chrome/defaults/preferences/prefs.js
@@ -0,0 +1,9 @@
+pref("extensions.sealu.preflanguage", "Thai"); 
+// the above should not be translated.
+//pref("extensions.sealu@sealang.net.description", "chrome://sealu/locale/main.properties"); 
+//pref("extensions.sealu@sealang.net.name", "chrome://sealu/locale/main.properties");
+//pref("extensions.sealu@sealang.net.creator", "chrome://sealu/locale/main.properties");
+// the preceding prefs allow the description and name to be localized in the Firefox add-ons window. Do not change them.
+// the pref below should be uncommented for localization translator(s)
+// pref("extensions.sealu@sealang.net.translator.1", "chrome://sealu/locale/main.properties");
+
